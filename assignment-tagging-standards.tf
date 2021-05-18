c1b43f21526cd1d93e17311c1b4b09fde69eaaf9
Justin OConnor <jsoconno@gmail.com>
2021-05-18 17:02:45 -0400

    update scope to be more generic for policy assignment

--- assignment-tagging-standards.tf
+++ assignment-tagging-standards.tf
@@ -1,6 +1,6 @@
 resource "azurerm_policy_assignment" "apa_tagging_standards" {
     name                 = "apa_tagging_standards"
-    scope                = "/subscriptions/c72acede-d539-45d9-963d-3464ec4ddae0/resourceGroups/RGP-USE-CORE-EXAMPLE-DV" #azurerm_management_group.tenant_root.id
+    scope                = data.azurerm_management_group.tenant_root.id
     policy_definition_id = azurerm_policy_set_definition.tagging_standards.id
     description          = ""
     display_name         = "Example Tagging Standards"
